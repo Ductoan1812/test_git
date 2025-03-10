a464116a3ab909a5204e00438f17b5c05927d1ac
DucToan1812 <Toan.tq.hy.2003@gmail.com>
2025-03-10 21:03:26 +0700

    edit file ductoan

--- ductoan.cpp
+++ ductoan.cpp
@@ -1,2 +1,8 @@
-hay ho ghe
-ductoáds
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    cout << "hello word";
+    cout << "acc ductoanvps24";
+    return 0;
+}
